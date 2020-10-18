65bd06e2f282e3bee226c90f59e4ede7766c0e7a
Sun Keun Choi <sunkeunchoi@MacBook-Pro.local>
2020-10-19 00:42:36 +0900

    nest configuration done

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common'
-
+import { ConfigModule } from '@nestjs/config'
+import { configuration } from './config/configuration'
+import { validationSchema } from './config/validationSchema'
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
